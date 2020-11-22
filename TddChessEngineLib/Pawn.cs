49bbcba70edd84076409a8b5412b3ec7d5ecebf5
BiBa <crotow.tony@gmail.com>
2020-11-22 11:56:50 +0300

    add figureColor

--- TddChessEngineLib/Pawn.cs
+++ TddChessEngineLib/Pawn.cs
@@ -5,22 +5,30 @@ namespace TddChessEngineLib
     public class Pawn
     {
         public string CurrentPosition {get; private set;}
+        public FigureColor FigureColor {get;}
 
-        public Pawn(string initialPosition)
+        public Pawn(string initialPosition, FigureColor figureColor)
         {
             CurrentPosition = initialPosition;
+            FigureColor = figureColor;
         }
 
+        // возможность первого хода
+        // 1. Если кто-то в точке назначения
+        // 2. Граница карты
+        // 3. Направления
+        // 4. Ввести переменную сходила ли пешка или нет
+        // 5. (жива или нет)
+        // 6. Длина шага 2 или 1
         public void Turn(string startPosition, string finalPosition)
         {
-            if (startPosition == CurrentPosition)
-            {
-                CurrentPosition = finalPosition;
-            }
-            else
-            {
-                throw new ArgumentException($"Current position is {CurrentPosition}");
-            }
+            CurrentPosition = finalPosition;
         }
+    }
+
+    public enum FigureColor
+    {
+        White,
+        Black
     }
 }
